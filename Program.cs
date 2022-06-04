4071057f5cb410d714cd2cd0c5e9a6059faef702
Artem Ignatov <ghettoarabdog@gmail.com>
2022-06-04 16:37:21 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_Mod_3._10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число:");
+            int num = int.Parse(Console.ReadLine());
+            if (num%2 == 0)
+            {
+                Console.WriteLine("Введенное Вами число является чётным.");
+            }
+            else
+            {
+                Console.WriteLine("Введенное Вами число является нечётным.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
